04fec84288c1401f7da26b83df7628fab31a8bf3
TuPhamTP <phamlemongtu@gmail.com>
2024-09-15 21:43:38 +0700

    [fix code - update]

--- Assets/_mapping/Scripts/InputController.cs
+++ Assets/_mapping/Scripts/InputController.cs
@@ -9,7 +9,7 @@ public class InputController : MonoBehaviour
     [DllImport("__Internal")] private static extern void OpenWebsite();
     [DllImport("__Internal")] private static extern int GetDeviceType();
 
-    //[SerializeField] private PinchZoom _pinchZoom;
+    [SerializeField] private GameObject _startStation;
     [SerializeField] private SpriteRenderer[] _roadSRs;
     [SerializeField] private SpriteRenderer[] _dotSRs;
     [SerializeField] private SpriteRenderer[] _txtStationSRs;
@@ -21,8 +21,8 @@ public class InputController : MonoBehaviour
     [SerializeField] private GameObject _arrow, _tapIcon;
     [SerializeField] private Transform _tapIconStart, _tapIconEnd;
     [SerializeField] private GameObject _panelMap;
-    [SerializeField] private GameObject _txtStart;
-    public Text _txtDevice;
+
+    [SerializeField] private Text _txtDevice, _txtOrthographicsSize;
 
     private List<Sprite> _currentTxtStations = new List<Sprite>();
     private List<Sprite> _currentTxtStationBlurs = new List<Sprite>();
@@ -33,47 +33,43 @@ public class InputController : MonoBehaviour
     private Vector2 _minBgBound, _maxBgBound;
     private int _currentID = -1;
     private bool _showStation6First;
+    private bool _canInteractMap;
     private bool _playArrow, _playTapIcon;
     private bool _openedWebsite;
-    private bool _canDrag;
-    private bool _isWebGL;
+    private bool _isWebGL, _isPhone;
 
-    float camHeight;
-    float camWidth;
-    SpriteRenderer backgroundSprite;
-    Vector2 backgroundSize;
+    #region CalculateBgBound
+    private float _camHeight;
+    private float _camWidth;
+    private Vector2 _backgroundSize;
 
-    private void CalculateBound()
+    private void CalculateBgBound()
     {
         // Get the camera's size based on its orthographic size
-        camHeight = _mainCamera.orthographicSize * 2;
-        camWidth = camHeight * _mainCamera.aspect;
+        _camHeight = _mainCamera.orthographicSize * 2;
+        _camWidth = _camHeight * _mainCamera.aspect;
 
         // Calculate the minimum and maximum boundaries the camera can move
-        _minBgBound = (Vector2)_background.position - backgroundSize / 2 + new Vector2(camWidth / 2, camHeight / 2);
-        _maxBgBound = (Vector2)_background.position + backgroundSize / 2 - new Vector2(camWidth / 2, camHeight / 2);
+        _minBgBound = (Vector2)_background.position - _backgroundSize / 2 + new Vector2(_camWidth / 2, _camHeight / 2);
+        _maxBgBound = (Vector2)_background.position + _backgroundSize / 2 - new Vector2(_camWidth / 2, _camHeight / 2);
     }
+    #endregion
 
     void Start()
     {
         _isWebGL = Application.platform == RuntimePlatform.WebGLPlayer;
         if (_isWebGL)
         {
-            bool _isPhone = (GetDeviceType() == 1) ? true : false;
+            _isPhone = (GetDeviceType() == 1) ? true : false;
             _txtDevice.text = _isPhone ? "Phone" : "Computers";
         }
 
         DOVirtual.DelayedCall(0.2f, () =>
         {
             // Calculate the background bounds based on the SpriteRenderer size
-            backgroundSprite = _background.GetComponent<SpriteRenderer>();
-            backgroundSize = backgroundSprite.bounds.size;
+            _backgroundSize = _background.GetComponent<SpriteRenderer>().bounds.size;
+            CalculateBgBound();
         });
-        
-
-        CalculateBound();
-
-        //_mainCamera.transform.position = _startCameraPos;
 
         for (int i = 0; i < _txtStationViewMoreBlurs.Length; i++)
         {
@@ -94,23 +90,24 @@ public class InputController : MonoBehaviour
         _tapIconDirection = (_tapIconEnd.position - _tapIconStart.position).normalized;
         _tapIcon.transform.position = _tapIconStart.position;
 
-        _txt.text = mainCamera.orthographicSize.ToString();
+        _txtOrthographicsSize.text = _mainCamera.orthographicSize.ToString();
     }
 
+    #region ClickBtn
     public void ClickSeeDetails()
     {
         DOVirtual.DelayedCall(0.1f, () =>
         {
             _panelMap.SetActive(false);
-            _canDrag = true;
-            _txtStart.transform.localScale = Vector3.zero;
-            _txtStart.transform.DOScale(0.39768f, 1f);
+            _canInteractMap = true;
+            _startStation.transform.localScale = Vector3.zero;
+            _startStation.transform.DOScale(0.39768f, 1f);
         });
-        
+
     }
 
     public void CliclReadMore()
-    { 
+    {
         if (_currentID == _txtStationSRs.Length - 1)
         {
             if (_isWebGL) { OpenWebsite(); }
@@ -131,11 +128,13 @@ public class InputController : MonoBehaviour
             _txtStationSRs[indexStation].sprite = _currentTxtStations[indexStation];
         }
     }
+    #endregion
 
     void Update()
     {
-        if (_canDrag)
+        if (_canInteractMap)
         {
+            CalculateBgBound();
             CheckInput();
             CheckPosition();
 
@@ -160,8 +159,9 @@ public class InputController : MonoBehaviour
         
     }
 
-    Vector3 _worldPositionChecker;
-    Vector3 _textWorldPos;
+    #region CheckPositionToBlur_ShowText
+    private Vector3 _worldPositionChecker;
+    private Vector3 _textWorldPos;
     private void CheckPosition()
     {
         for (int i = 0; i < _txtStationSRs.Length; i++)
@@ -177,6 +177,7 @@ public class InputController : MonoBehaviour
             }
         }
     }
+    #endregion
 
     void UpdateSprites(int newIndex)
     {
@@ -218,97 +219,131 @@ public class InputController : MonoBehaviour
         else { _tapIcon.SetActive(false); }
     }
 
-    private Vector3 _mousePos;
-    private Vector3 _originalDistance;
     private void CheckInput()
     {
-        //_txt2.text = (Input.touchCount == 1).ToString() + "--" + ()
-        if ((Input.touchCount == 1))// || Input.GetMouseButton(0)
+        if (_isPhone)
         {
-            if (_canTouch1)
+            if (Input.touchCount == 1)
             {
-                isZooming = false;
-                CalculateBound();
-                // Start dragging
-                if (Input.GetMouseButtonDown(0))
-                {
-                    _dragOrigin = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
-                }
-
-                // Continue dragging
-                if (Input.GetMouseButton(0))
-                {
-                    Vector3 difference = _dragOrigin - _mainCamera.ScreenToWorldPoint(Input.mousePosition);
-                    _mainCamera.transform.position = ClampCamera(_mainCamera.transform.position + difference);
+                if (_canTouchOne) 
+                { 
+                    _isZoomingByFingers = false; 
+                    Drag(); 
                 }
             }
-            
-        }
-        else if (Input.touchCount == 2)
-        {
-            Zoom();
-        }
-        else { isZooming = false; }
-            
-    }
-
-    public Camera mainCamera;
-    public Text _txt;
-
-    private float zoomSpeed = 0.0001f;
-    private float minZoom = 1f;
-    private float maxZoom = 5f;
-
-    private bool isZooming = false;
-    private float initialTouchDistance;
-    private float zoomThreshold = 0.1f;
-
-    public void Zoom()
-    {
-        Touch touchZero = Input.GetTouch(0);
-        Touch touchOne = Input.GetTouch(1);
-
-        float currentTouchDistance = (touchZero.position - touchOne.position).magnitude;
-
-        if (!isZooming)
-        {
-            initialTouchDistance = currentTouchDistance;
-            isZooming = true;
+            else if (Input.touchCount == 2)
+            {
+                ZoomWithFingers();
+            }
+            else { _isZoomingByFingers = false; }
         }
         else
         {
-            float distanceDifference = Mathf.Abs(currentTouchDistance - initialTouchDistance);
+            Drag();
+            ZoomWithMouse();
+        }
+          
+    }
 
-            if (distanceDifference > zoomThreshold)
+    #region Drag
+    private Vector3 _dragDifference;
+
+    private void Drag()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _dragOrigin = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            _dragDifference = _dragOrigin - _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            _mainCamera.transform.position = ClampCamera(_mainCamera.transform.position + _dragDifference);
+        }
+    }
+    #endregion
+
+    private float _minZoom = 1f;
+    private float _maxZoom = 5f;
+
+    #region ZoomWithFingers
+    private float _zoomSpeedByFingers = 0.0001f;
+    private bool _isZoomingByFingers = false;
+    private float _initialTouchDistance;
+    private float _currentTouchDistance;
+    private float _distanceDifference;
+    private float _deltaMagnitudeDiff;
+    private float _zoomThreshold = 0.1f;
+    private Touch _touchZero;
+    private Touch _touchOne;
+
+    public void ZoomWithFingers()
+    {
+        _touchZero = Input.GetTouch(0);
+        _touchOne = Input.GetTouch(1);
+        _currentTouchDistance = (_touchZero.position - _touchOne.position).magnitude;
+
+        if (!_isZoomingByFingers)
+        {
+            _initialTouchDistance = _currentTouchDistance;
+            _isZoomingByFingers = true;
+        }
+        else
+        {
+            _distanceDifference = Mathf.Abs(_currentTouchDistance - _initialTouchDistance);
+
+            if (_distanceDifference > _zoomThreshold)
             {
-                float deltaMagnitudeDiff = initialTouchDistance - currentTouchDistance;
+                _deltaMagnitudeDiff = _initialTouchDistance - _currentTouchDistance;
 
-                mainCamera.orthographicSize += deltaMagnitudeDiff * zoomSpeed;
+                _mainCamera.orthographicSize += _deltaMagnitudeDiff * _zoomSpeedByFingers;
 
-                mainCamera.orthographicSize = Mathf.Clamp(mainCamera.orthographicSize, minZoom, maxZoom);
+                _mainCamera.orthographicSize = Mathf.Clamp(_mainCamera.orthographicSize, _minZoom, _maxZoom);
             }
         }
 
-        _txt.text = mainCamera.orthographicSize.ToString() + "-" + (Input.touchCount == 2).ToString();
-
-        if (touchZero.phase == TouchPhase.Ended || touchOne.phase == TouchPhase.Ended ||
-            touchZero.phase == TouchPhase.Canceled || touchOne.phase == TouchPhase.Canceled)
+        if (_touchZero.phase == TouchPhase.Ended || _touchOne.phase == TouchPhase.Ended ||
+            _touchZero.phase == TouchPhase.Canceled || _touchOne.phase == TouchPhase.Canceled)
         {
-            isZooming = false;
-            _canTouch1 = false;
-            Invoke(nameof(SetTouch1), 0.2f);
+            _isZoomingByFingers = false;
+            _canTouchOne = false;
+            Invoke(nameof(SetTouchOne), 0.2f);
         }
+
+        _txtOrthographicsSize.text = _mainCamera.orthographicSize.ToString() + "-" + (Input.touchCount == 2).ToString();
     }
 
-    bool _canTouch1 = true;
-    private void SetTouch1() { _canTouch1 = true; }
+    private bool _canTouchOne = true;
+    private void SetTouchOne() { _canTouchOne = true; }
+    #endregion
 
+    #region ZoomWithMouse
+    private float _zoomSpeedByMouse = 0.25f;
+    private float _scrollDelta;
+
+    public void ZoomWithMouse()
+    {
+        _scrollDelta = Input.mouseScrollDelta.y;
+
+        if (_scrollDelta != 0)
+        {
+            _mainCamera.orthographicSize -= _scrollDelta * _zoomSpeedByMouse;
+
+            _mainCamera.orthographicSize = Mathf.Clamp(_mainCamera.orthographicSize, _minZoom, _maxZoom);
+        }
+
+        _txtOrthographicsSize.text = _mainCamera.orthographicSize.ToString();
+    }
+    #endregion
+
+    #region ClampCamera
     // Clamp the camera position to the background bounds
     float _clampedX, _clampedY;
     float _maxClampedY = 5;
+
     private Vector3 ClampCamera(Vector3 targetPosition)
     {
         _clampedX = Mathf.Clamp(targetPosition.x, _minBgBound.x, _maxBgBound.x);
+        /*
         if (!_showStation6First)
         {
             if (_mainCamera.transform.position.x > 1)
@@ -321,7 +356,12 @@ public class InputController : MonoBehaviour
         {
             _clampedY = Mathf.Clamp(targetPosition.y, _minBgBound.y, _maxBgBound.y);
         }
-        
+        */
+        _clampedY = Mathf.Clamp(targetPosition.y, _minBgBound.y, _maxBgBound.y);
+
         return new Vector3(_clampedX, _clampedY, targetPosition.z);
     }
+    #endregion
+
+
 }
