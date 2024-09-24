24fc310d32782be7a193c9e4ed083418683b81fc
TuPhamTP <phamlemongtu@gmail.com>
2024-09-24 22:54:32 +0700

    [update popup textures]

--- Assets/_mapping/Scripts/PopUpText.cs
+++ Assets/_mapping/Scripts/PopUpText.cs
@@ -6,18 +6,13 @@ public class PopUpText : MonoBehaviour
 {
     [SerializeField] private GameObject _panelPopUp;
     [SerializeField] private Transform _popUp;
-    [SerializeField] private Text _txtBtnBack;
+    [SerializeField] private Sprite[] _popUpENs;
+    [SerializeField] private Sprite[] _popUpVNs;
+    [SerializeField] private Image _popUpImg;
+    //[SerializeField] private Text _txtBtnBack;
 
-    [SerializeField] private Text _txtNumber;
-    [SerializeField] private Text _txtTitle;
-    [SerializeField] private Text _txtContent;
+    private Sprite[] _currentPopUpSprites = new Sprite[8];
 
-    [SerializeField] private string[] _txtTitleENs, _txtTitleVNs;
-    [SerializeField] private string[] _txtContentENs, _txtContentVNs;
-
-    private string[] _currentTxtNumbers = { "01", "02", "03", "04", "05", "06", "07", "08" };
-    private string[] _currentTxtTitles = new string[8];
-    private string[] _currentTxtContents = new string[8];
 
     void Start()
     {
@@ -31,27 +26,23 @@ public class PopUpText : MonoBehaviour
         {
             for (int i = 0; i < 8; i++)
             {
-                _currentTxtTitles[i] = _txtTitleENs[i];
-                _currentTxtContents[i] = _txtContentENs[i];
+                _currentPopUpSprites[i] = _popUpENs[i];
             }
-            _txtBtnBack.text = "Back";
+            //_txtBtnBack.text = "Back";
         }
         else
         {
             for (int i = 0; i < 8; i++)
             {
-                _currentTxtTitles[i] = _txtTitleVNs[i];
-                _currentTxtContents[i] = _txtContentVNs[i];
+                _currentPopUpSprites[i] = _popUpVNs[i];
             }
-            _txtBtnBack.text = "Quay lại";
+            //_txtBtnBack.text = "Quay lại";
         }
     }
 
     public void ShowPopUpText(int stationIndex)
     {
-        _txtNumber.text = _currentTxtNumbers[stationIndex];
-        _txtTitle.text = _currentTxtTitles[stationIndex];
-        _txtContent.text = _currentTxtContents[stationIndex];
+        _popUpImg.sprite = _currentPopUpSprites[stationIndex];
 
         _popUp.localScale = Vector3.zero;
         _popUp.DOScale(1, 0.7f);
